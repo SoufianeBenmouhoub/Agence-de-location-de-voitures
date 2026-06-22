1cc09adaf29c5ce640b5a98959ad3ab8b7fc9275
SoufianeBenmouhoub <soufianebenmouhoub@gmail.com>
2026-06-22 16:18:25 +0200

    Initial project setup and database configuration

---
+++ Locatic/Models/Reservation.cs
@@ -0,0 +1,18 @@
+namespace Locatic.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+
+    public DateTime DateDebut { get; set; }
+
+    public DateTime DateFin { get; set; }
+
+    public int ClientId { get; set; }
+
+    public Client? Client { get; set; }
+
+    public int VoitureId { get; set; }
+
+    public Voiture? Voiture { get; set; }
+}
---
+++ Locatic/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Locatic.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=locatic.db");
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
